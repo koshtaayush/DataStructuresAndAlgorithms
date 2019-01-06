40d22185fef7b4fd9b4e8fcbb450a19a488fa642
Ayush Koshta <koshtaayush@gmail.com>
2019-01-06 19:27:29 +0530

    Create Introduction.js

---
+++ Trees/Introduction.js
@@ -0,0 +1,6 @@
+Why use trees ?
+
+1. Stores hierachical information
+2. Trees (with some ordering e.g., BST) provide moderate access/search (quicker than Linked List and slower than arrays).
+3. Trees provide moderate insertion/deletion (quicker than Arrays and slower than Unordered Linked Lists).
+4. Like Linked Lists and unlike Arrays, Trees don’t have an upper limit on number of nodes as nodes are linked using pointers.

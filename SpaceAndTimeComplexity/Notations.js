056898dcd30fef41a44239555ccc40c5961a3f85
Ayush Koshta <koshtaayush@gmail.com>
2019-01-06 20:01:36 +0530

    Create Notations.js
    The notations about different complexities present.

---
+++ SpaceAndTimeComplexity/Notations.js
@@ -0,0 +1,29 @@
+ *     Name           Notation     How you feel when they show up at your party
+ *     ------------------------------------------------------------------------
+ *     Constant       O(1)         AWESOME!!
+ *     Logarithmic    O(log N)     GREAT!
+ *     Linear         O(N)         OKAY.
+ *     Linearithmic   O(N log N)   UGH...
+ *     Polynomial     O(N ^ 2)     SHITTY
+ *     Exponential    O(2 ^ N)     HORRIBLE
+ *     Factorial      O(N!)        WTF
+ 
+ 
+ 
+ * It is important to note that data structures may be good at one action but
+ * bad at another.
+ *
+ *                            Accessing    Searching    Inserting    Deleting
+ *    -------------------------------------------------------------------------
+ *                  Array     O(1)         O(N)         O(N)         O(N)
+ *            Linked List     O(N)         O(N)         O(1)         O(1)
+ *     Binary Search Tree     O(log N)     O(log N)     O(log N)     O(log N)
+ 
+ 
+ 
+ *                            Accessing    Searching    Inserting    Deleting
+ *    -------------------------------------------------------------------------
+ *                  Array     AWESOME!!    OKAY         OKAY         OKAY
+ *            Linked List     OKAY         OKAY         AWESOME!!    AWESOME!!
+ *     Binary Search Tree     GREAT!       GREAT!       GREAT!       GREAT!
+ 

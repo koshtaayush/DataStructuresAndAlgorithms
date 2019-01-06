bd7bdd8b723368405e178293a748ed4c74c249a0
Ayush Koshta <koshtaayush@gmail.com>
2019-01-06 20:22:19 +0530

    Update Introduction.js
    Properties of binary tree

--- Trees/Introduction.js
+++ Trees/Introduction.js
@@ -4,3 +4,29 @@ Why use trees ?
 2. Trees (with some ordering e.g., BST) provide moderate access/search (quicker than Linked List and slower than arrays).
 3. Trees provide moderate insertion/deletion (quicker than Arrays and slower than Unordered Linked Lists).
 4. Like Linked Lists and unlike Arrays, Trees don’t have an upper limit on number of nodes as nodes are linked using pointers.
+
+
+Main applications of trees include:
+
+1. Manipulate hierarchical data.
+2. Make information easy to search (see tree traversal).
+3. Manipulate sorted lists of data.
+4. As a workflow for compositing digital images for visual effects.
+5. Router algorithms
+6. Form of a multi-stage decision-making (see business chess). 
+
+
+Properties of Binary tree:
+
+1) The maximum number of nodes at level ‘l’ of a binary tree is 2^(l-1).
+- The level of root is 1
+
+2) Maximum number of nodes in a binary tree of height ‘h’ is 2^(h) – 1.
+- Here height of a tree is maximum number of nodes on root to leaf path. Height of a tree with single node is considered as 1.
+- If height of leaf is considered as 0 then, the above formula becomes 2^(h+1) – 1.
+
+3) In a Binary Tree with N nodes, minimum possible height or minimum number of levels is  ⌈ Log2(N+1) ⌉   
+
+4) A Binary Tree with L leaves has at least   ⌈ Log2L ⌉ + 1   levels
+
+5) In Binary tree where every node has 0 or 2 children, number of leaf nodes is always one more than nodes with two children.

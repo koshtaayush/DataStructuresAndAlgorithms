0088b4e791234dcf79ca16a28660d053619da957
Ayush Koshta <koshtaayush@gmail.com>
2019-01-06 19:49:10 +0530

    Update SortingAlgos.js

--- SpaceAndTimeComplexity/SortingAlgos.js
+++ SpaceAndTimeComplexity/SortingAlgos.js
@@ -1,1 +1,28 @@
+Sorting	      Worst 	  Average	    Best
+---------------------------------------------
+Bubble 	      N2        N2          N
+Insetion      N2        N2          N
+Heap          nlogn     nlogn       nlogn
+Merge         nlogn     nlogn       nlogn
+Selection     n2        n2          n2
+Quick         n2        nlogn       nlogn
 
+
+**********************************************
+
+The words you have to remember are
+1. BIn
+-  Both Bubble and Insertion sort have same complexity
+-  Best Is n
+
+2. HtMI
+-  Heap Sort and Merge Sort
+-  I stands for nlogn
+
+3. S
+- Selection sort 
+- S stands for square
+- All are n2
+
+4. Qsl(Queen of  scott-land)
+- First column of quick sort is n2(as "s" in scott) and rest are nlong (as l in land).

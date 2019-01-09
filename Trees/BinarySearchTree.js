0c483982032fba39c43abd98cab7f9ad02ab2985
Ayush Koshta <koshtaayush@gmail.com>
2019-01-09 23:28:17 +0530

    BST Structure class

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,17 @@
+//Node 
+
+class Node {
+  constructor(data){
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null
+  }
+}
+
+var BST = new BinarySearchTree();

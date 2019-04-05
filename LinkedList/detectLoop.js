d211628c96bd2e87f227a0500cd5484eb7ba2a71
Ayush Koshta <koshtaayush@gmail.com>
2019-04-05 09:08:15 +0530

    detect a linked list contains loop or not

---
+++ LinkedList/detectLoop.js
@@ -0,0 +1,77 @@
+class Node{
+    constructor(element){
+        this.element = element ;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+
+    //adding an element in the linked list
+    add(element){
+        var newNode = new Node(element);
+
+        var current;
+
+        if(this.head == null){
+            this.head = newNode;
+        }else{
+            current = this.head;
+            while(current.next){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        this.size++;
+
+    }
+
+    //printing the linked list
+    printList(){
+        
+        var curr = this.head;
+        var str = "";
+
+        //To iterate over the list use curr != null
+        while(curr != null){
+            str += " " +curr.element;
+            curr = curr.next;
+        }
+        console.log(str);
+    }
+
+    detectLoop(){
+        var slowPtr = thi.head;
+        var fastPtr = this.head;
+
+        while(fastPtr != null && fastPtr.next != null && slowPtr != null){
+            if(slowPtr == fastPtr){
+                console.log("loop detected")
+                return 1;
+            }
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+        }
+
+        return 0;
+    }
+    
+}
+
+
+var l1 = new LinkedList();
+
+l1.add(10);
+l1.add(20);
+l1.add(30);
+l1.add(40);
+l1.add(50);
+l1.add(60);
+
+l1.printList();
